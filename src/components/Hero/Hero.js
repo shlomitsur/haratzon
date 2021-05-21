812d2c9c47e6cc3c37f87deaba39f7d1d64f2800
shlomi <shlomo.tsur@gmail.com>
2021-05-21 13:45:38 +0300

    fils

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -28,7 +28,6 @@ const Hero = props => {
           display: flex;
           flex-flow: column nowrap;
           justify-content: center;
-          // min-height: 100vh;
           height: 100px;
           padding: ${theme.space.inset.l};
           padding-top: ${theme.header.height.homepage};
